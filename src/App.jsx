d4ad20840fb4839365821f94546e1193ec3a535d
julius <nyerereoffice@gmail.com>
2024-04-03 10:29:16 +0300

    Single JobListing Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
-      <HomeCards/>    
+      <HomeCards/>   
+      <JobListings/> 
     </>
   )
 }
