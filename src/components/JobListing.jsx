46102f4439c7cb0a51dac6ac6aca77fa793be7ad
julius <nyerereoffice@gmail.com>
2024-04-03 11:56:22 +0300

    Toggle and Updating State Component

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,18 @@
-/* eslint-disable react/prop-types */
-import React from 'react'
+import React from 'react';
+
+import { useState } from "react";
+
 
 const JobListing = ({ job }) => {
+
+    const [showFullDescription, setShowFullDescription] = useState(false)
+
+    let description = job.description
+
+    if (!showFullDescription) {
+        description = description.substring(0, 90) + '...';
+    }
+
     return (
         <div>
             <div className="bg-white rounded-xl shadow-md relative">
@@ -14,8 +25,12 @@ const JobListing = ({ job }) => {
                     </div>
 
                     <div className="mb-5">
-                        {job.description}
+                        {description}
                     </div>
+
+                    <button onClick={()=> setShowFullDescription((prevState)=> !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600 transition duration-500">
+                        {showFullDescription ? 'Less' : 'More'}
+                    </button>
 
                     <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
